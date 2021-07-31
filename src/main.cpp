084457b1a000d832863a4feb4999d5c2f00be59a
Capt Weiss <captweiss@foxmail.com>
2021-07-31 14:11:40 +0100

    modified to be operated segmentally
    Signed-off-by: Capt Weiss <captweiss@foxmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -10,460 +10,105 @@
   PF = Pattern Flow
 
   created 2021
-  by Kolade O. Adetoyinbo (Capt. Weiss)
+  modified 31-07-2021
+  Kolade O. Adetoyinbo [https://github.com/CaptWeiss](Capt. Weiss)
 
 */
 #include <Arduino.h>
 #define FASTLED_INTERNAL
 #include <FastLED.h>
+#include <main.h>
 
 #define DATA_PIN 13
-#define PIXELS 126      // 56 //126
-unsigned int sensePINs; // 11 sensors are connected to these PINs
-unsigned int PINs;      // stores the PIN checks at interval
-bool started = false;
+#define NUM_LEDS 126      // 56 //126
+#define COLOR_POT A1
 
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0; // will store last time LED was updated
+uint8_t sensePINs; // this persist input state to check for posible changes in the next input pin reads
+bool started = false; // used to decide a specific operation on first power
 
-// constants won't change :
-const int interval = 4000;
 
-// LEDs configuration
+// Define Connections to 74HC165
+// PL pin 1
+#define load 7
+// CE pin 15
+#define clockEnablePin 4
+// Q7 pin 7
+#define dataIn 5
+// CP pin 2
+#define clockIn 6
 
-uint8_t G1 = 20;              // 8 ; // 20
-uint8_t G2 = 46;              // 20 ; //46
-uint8_t G3[3] = {20, 20, 20}; // {8, 8, 8} ; //{20, 20, 20}
 
-// Do not change this setting
-int G100 = G1 / 2;
-int G101 = G1;
-int G200 = G101 + G2 / 4;
-int G201 = G101 + G2 / 2;
-int G202 = G101 + ((G2 / 4) * 3);
-int G203 = G101 + G2;
-int G300 = G203 + (G3[0] / 2);
-int G301 = G203 + G3[0];
-int G310 = G301 + (G3[1] / 2);
-int G311 = G301 + G3[1];
-int G320 = G311 + (G3[2] / 2);
-int G321 = G311 + G3[2];
+uint8_t S1[2] = {0, 20} ; // {segmentStartPixel, segmentLength}
+uint8_t S2[2] = {S1[1], 46} ;
+uint8_t S3[2] = {S1[1]+S2[1], 60} ;
+
+boolean AllSeg[3] = {false, false, false} ;
+uint8_t SegMask[3] = {B11000000,B00111100,B00000011} ;
+
+
+volatile uint8_t favColor = 120 ;
+volatile uint8_t favSaturation = 255;
+uint8_t brigtness = 255 ;
 
 // Define the array of leds
-CRGB leds[PIXELS];
+CRGB leds[NUM_LEDS];
 
-/**
- * Checks the state of all the inputs from D2 ~ D12 and marges it together.
- * This gives us the status of all the inputs
- */
-unsigned int checkSensePINs()
-{
-  // loop from the lowest pin to the highest:
-  if (!started)
-  {
-    Serial.println("Checking Sensor PINs...");
-    started = true;
+
+void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  // 74HC165 pins
+  pinMode(load, OUTPUT);
+  pinMode(clockEnablePin, OUTPUT);
+  pinMode(clockIn, OUTPUT);
+  pinMode(dataIn, INPUT);
+
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); // GRB ordering is typical
+  FastLED.setBrightness(brigtness);
+  readAnalogPin();
+  if(!started) {
+    intro();
+    started=true ;
   }
-  uint8_t a = PIND; // read Digital PIN 0 ~ 7
-  uint8_t b = PINB; // read Digital PIN 8 ~ 15
-
-  a >>= 4; // Discard PIN 0 & 1 as they are the Rx and Tx respectively and we do not need their state ; 00001111
-  b <<= 3; // Discard the 3 MSB(Most Significant Bits). The two MSB are D14 and D15 doesn't exist and the next(3rd) MSB, D13, is configured as Output.; 11111000
-
-  unsigned int c = b;
-
-  c = (c << 1) | a; //Merge both pins together
-  return c;
-}
-
-int handleInputChange(uint8_t pin, bool state)
-{
-  switch (pin)
-  {
-  // Group1
-  case 2:
-    if (state)
-    {
-      //start LED sequence
-      // wakeupBlink
-      for (uint8_t i = 0; i < 3; i++)
-      {
-        leds[(G1 / 2) - 1] = CRGB::Red;
-        FastLED.show();
-        delay(100);
-        leds[(G1 / 2) - 1] = CRGB::Black;
-        FastLED.show();
-        delay(100);
-      }
-      // Turn the LEDs on
-      for (int i = (G1 / 2) - 1; i >= 0; i--)
-      {
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(100);
-      }
-    }
-    else
-    {
-      // terminate LED sequence
-      // Turn the LEDs off
-      for (uint8_t i = 0; i < (G1 / 2); i++)
-      {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(100);
-      }
-    }
-    break;
-
-  case 3:
-    if (state)
-    {
-      //start LED sequence
-      // wakeupBlink
-      for (uint8_t i = 0; i < 3; i++)
-      {
-        leds[G1 / 2] = CRGB::Red;
-        FastLED.show();
-        delay(100);
-        leds[G1 / 2] = CRGB::Black;
-        FastLED.show();
-        delay(100);
-      }
-      // Turn the LEDs on
-      for (uint8_t i = (G1 / 2); i < G1; i++)
-      {
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(100);
-      }
-    }
-    else
-    {
-      // terminate LED sequence
-      // Now turn the LED off, then pause
-      for (uint8_t i = G1 - 1; i >= (G1 / 2); i--)
-      {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(100);
-      }
-    }
-    break;
-  // Group 2
-  case 4:
-    if (state)
-    {
-      //start LED sequence
-      // wakeupBlink
-      for (uint8_t i = 0; i < 3; i++)
-      {
-        leds[(G1 + (G2 / 4)) - 1] = CRGB::Red;
-        FastLED.show();
-        delay(50);
-        leds[(G1 + (G2 / 4)) - 1] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-      // Turn the LEDs on
-      for (int i = (G1 + (G2 / 4)) - 1; i >= G1; i--)
-      {
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(100);
-        Serial.println(i);
-      }
-    }
-    else
-    {
-      // terminate LED sequence
-      // Turn the LEDs off
-      for (uint8_t i = G1; i < (G1 + (G2 / 4)); i++)
-      {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(100);
-      }
-    }
-    break;
-
-  case 5:
-    if (state)
-    {
-      //start LED sequence
-      // wakeupBlink
-      for (uint8_t i = 0; i < 3; i++)
-      {
-        leds[G1 + (G2 / 4)] = CRGB::Red;
-        FastLED.show();
-        delay(50);
-        leds[G1 + (G2 / 4)] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-      // Turn the LEDs on
-      for (uint8_t i = (G1 + (G2 / 4)); i < G1 + (G2 / 2); i++)
-      {
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(100);
-      }
-    }
-    else
-    {
-      // terminate LED sequence
-      // Now turn the LED off, then pause
-      for (uint8_t i = G1 + (G2 / 2) - 1; i >= (G1 + (G2 / 4)); i--)
-      {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(100);
-      }
-    }
-    break;
-
-  case 6:
-    if (state)
-    {
-      //start LED sequence
-      // wakeupBlink
-      for (uint8_t i = 0; i < 3; i++)
-      {
-        leds[(G1 + ((G2 / 4) * 3)) - 1] = CRGB::Red;
-        FastLED.show();
-        delay(50);
-        leds[(G1 + ((G2 / 4) * 3)) - 1] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-      // Turn the LEDs on
-      for (int i = (G1 + ((G2 / 4) * 3)) - 1; i >= G1 + (G2 / 2); i--)
-      {
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(100);
-        Serial.println(i);
-      }
-    }
-    else
-    {
-      // terminate LED sequence
-      // Turn the LEDs off
-      for (uint8_t i = G1 + (G2 / 2); i < (G1 + ((G2 / 4) * 3)); i++)
-      {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(100);
-      }
-    }
-    break;
-
-  case 7:
-    if (state)
-    {
-      //start LED sequence
-      // wakeupBlink
-      for (uint8_t i = 0; i < 3; i++)
-      {
-        leds[(G1 + ((G2 / 4) * 3))] = CRGB::Red;
-        FastLED.show();
-        delay(100);
-        leds[(G1 + ((G2 / 4) * 3))] = CRGB::Black;
-        FastLED.show();
-        delay(100);
-      }
-      // Turn the LEDs on
-      for (uint8_t i = (G1 + ((G2 / 4) * 3)); i < G1 + G2; i++)
-      {
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(100);
-      }
-    }
-    else
-    {
-      // terminate LED sequence
-      // Now turn the LED off, then pause
-      for (uint8_t i = G1 + G2; i >= (G1 + ((G2 / 4) * 3)); i--)
-      {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(100);
-      }
-    }
-    break;
-    // Group 3
-
-  case 8:
-    if (state)
-    {
-      //start LED sequence
-      // wakeupBlink
-      for (uint8_t i = 0; i < 3; i++)
-      {
-        leds[G300 - 1] = CRGB::Red;
-        leds[G310 - 1] = CRGB::Green;
-        leds[G320 - 1] = CRGB::Purple;
-        FastLED.show();
-        delay(50);
-        leds[G300 - 1] = CRGB::Black;
-        leds[G310 - 1] = CRGB::Black;
-        leds[G320 - 1] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-      // Turn the LEDs on
-      for (uint8_t i = G300 - 1; i >= G203; i--)
-      { // LED_S_D: >>> | PF: <...<
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(50);
-      }
-      for (uint8_t i = G310; i < G311; i++)
-      { // LED_S_D: <<< | PF: <...<
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(50);
-      }
-      for (uint8_t i = G320 - 1; i >= G311; i--)
-      { // LED_S_D: >>> | PF: <...<
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(50);
-      }
-    }
-    else
-    {
-      // terminate LED sequence
-      // Turn the LEDs off
-      for (uint8_t i = G203; i < G300; i++)
-      {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-      for (uint8_t i = G311; i >= G310; i--)
-      {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-      for (uint8_t i = G311; i < G320; i++)
-      {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-    }
-    break;
-
-  case 9:
-    if (state)
-    {
-      //start LED sequence
-      // wakeupBlink
-      for (uint8_t i = 0; i < 3; i++)
-      {
-        leds[G300] = CRGB::Red;
-        leds[G310] = CRGB::Green;
-        leds[G320] = CRGB::Purple;
-        FastLED.show();
-        delay(50);
-        leds[G300] = CRGB::Black;
-        leds[G310] = CRGB::Black;
-        leds[G320] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-      // Turn the LEDs on
-      for (uint8_t i = G300; i < G301; i++)
-      { // LED_S_D: >>> | PF: >...>
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(50);
-      }
-      for (uint8_t i = G310 - 1; i > G301; i--)
-      { // LED_S_D: <<< | PF: >...>
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(50);
-      }
-      for (uint8_t i = G320; i < G321; i++)
-      { // LED_S_D: >>> | PF: >...>
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        delay(50);
-      }
-    }
-    else
-    {
-      // terminate LED sequence
-      // Turn the LEDs off
-      for (uint8_t i = G301; i >= G300; i--)
-      { // LED_S_D: >>> | PF: <...<
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-      for (uint8_t i = G301; i < G310; i++)
-      { // LED_S_D: <<< | PF: <...<
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-      for (uint8_t i = G321; i >= G320; i--)
-      { // LED_S_D: >>> | PF: <...<
-        leds[i] = CRGB::Black;
-        FastLED.show();
-        delay(50);
-      }
-    }
-    break;
-  }
-};
-
-void setup()
-{
-  DDRB = B11100000;
-  DDRD = DDRD | B00000000;                               // this is safer way to as it sets pins 2 to 7 as input without changing the value of pins 0 & 1, which are RX & TX
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, PIXELS); // GRB ordering is typical
-  FastLED.setBrightness(180);
-
-  Serial.begin(9600);
-  Serial.println("Reading inputs...");
-
-  Serial.print("PIND D0~D7 in BIN: ");
-  Serial.print(PIND, BIN);
-  Serial.print(" - DEC: ");
-  Serial.println(PIND);
-
-  Serial.print("PINB D8~D15 in BIN: ");
-  Serial.print(PINB, BIN);
-  Serial.print(" - DEC: ");
-  Serial.println(PINB);
-  Serial.println();
-  sensePINs = checkSensePINs();
-  PINs = sensePINs;
-  Serial.println();
   delay(5000);
-  Serial.println("************************ - STARTED - *****************************");
-  Serial.println();
 }
 
-void loop()
-{
-  PINs = checkSensePINs();
-  if (sensePINs ^ PINs)
-  { // Check if any of the bits(input state) has changed since last checked
+void loop() {
+
+  EVERY_N_MILLISECONDS(50) {
+    programme();
+  }
+
+  EVERY_N_MILLISECONDS(200) {
+    fadeOffDueSegment();
+    readAnalogPin();
+  }
+
+  EVERY_N_MINUTES(1) {
+    blinkIndicator();
+  }
+
+  FastLED.show();
+}
+
+
+
+void programme() {
+  // Get data from 74HC165
+  digitalWrite(clockIn, HIGH);
+  digitalWrite(clockEnablePin, LOW);
+  uint8_t incoming = shiftIn(dataIn, clockIn, LSBFIRST);
+  digitalWrite(clockEnablePin, HIGH);
+
+  if (sensePINs ^ incoming) { // Check if any of the bits(input state) has changed since last checked
     Serial.print("PINs state changed to - ");
-    Serial.print(PINs, BIN);
+    Serial.print(incoming, BIN);
     Serial.print("(");
-    Serial.print(PINs);
+    Serial.print(incoming);
     Serial.println(")");
-    for (uint8_t i = 0; i < 11; i++)
+    for (uint8_t i = 0; i < 8; i++)
     {
-      int n = PINs & (1 << i);
+      int n = incoming & (1 << i);
       int changedPIN = (sensePINs & (1 << i)) ^ n;
       if (changedPIN)
       {
@@ -474,17 +119,102 @@ void loop()
         handleInputChange(i + 2, n > 0);
       }
     }
-    sensePINs = PINs;
+    sensePINs = incoming;
   }
+}
 
-  //Check Sensors state and Blink LED
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval)
-  {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-    PORTB ^= B00100000;
-    delay(10);
-    PORTB ^= B00100000;
+void fadeOffDueSegment() {
+  for (uint8_t i = 0; i < 3; i++) {
+    if ((SegMask[i] & sensePINs) == 0)
+    { // this segment is off
+      switch (i)
+      {
+      case 0:
+        turnOff(S1[0], S1[1], i);
+        if (AllSeg[i])
+          AllSeg[i] = false;
+        break;
+      case 1:
+        turnOff(S2[0], S2[1], i);
+        if (AllSeg[i])
+          AllSeg[i] = false;
+        break;
+      case 2:
+        turnOff(S3[0], S3[1], i);
+        if (AllSeg[i])
+          AllSeg[i] = false;
+        break;
+      default:
+        break;
+      }
+    }
   }
+}
+
+void blinkIndicator() {
+  leds[S1[0]] = CRGB::Green;
+  FastLED.show();
+  FastLED.delay(200);
+  leds[S1[0]] = leds[S1[0] + 1];
+}
+
+void handleInputChange(uint8_t pin, bool state) {
+  switch (pin) {
+    case 0:
+    case 1:
+      if (state) turnOn(S1[0], S1[1], 0);
+      break;
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      if (state) turnOn(S2[0],S2[1],1);
+      break;
+    case 6:
+    case 7:
+      if (state) turnOn(S3[0],S3[1],2);
+      break;
+  }
+};
+
+void turnOn(uint8_t start, uint8_t leds_length, uint8_t segIdx) {
+  if(!AllSeg[segIdx]) {
+    for (int i = 0; i < brigtness; i++) {
+      fill_solid(leds+start, leds_length, CHSV(favColor, favSaturation, i));
+      FastLED.show();
+      FastLED.delay(10);
+    }
+    AllSeg[segIdx] = true ;
+  }
+}
+
+void turnOff(uint8_t start, uint8_t leds_length, uint8_t segIdx) {
+    fadeToBlackBy(leds + start, leds_length, 30);
+}
+
+void readAnalogPin() {
+  int analVal = analogRead(COLOR_POT);
+    if(analVal<=776){
+      favColor = map(analVal, 0, 776, 0, 255);
+    }
+    if(analVal>776) {
+      favSaturation = map(analVal, 777, 1023, 255, 0);
+    }
+}
+
+void intro() {
+  fill_solid(leds+S1[0],S1[1],CHSV(favColor-50,favSaturation,150)) ;
+  delay(2000);
+  FastLED.show();
+  fill_solid(leds+S2[0],S2[1],CHSV(favColor-100,favSaturation,150)) ;
+  delay(2000);
+  FastLED.show();
+  fill_solid(leds+S3[0],S3[1],CHSV(favColor-150,favSaturation,150)) ;
+  delay(2000);
+  FastLED.show();
+  for(uint8_t i = 0; i<100 ; i++) {
+    fadeToBlackBy(leds,NUM_LEDS,30);
+  }
+  fill_solid(leds,NUM_LEDS,CRGB::Black);
+  delay(2000);
 }
---
+++ src/main.h
@@ -0,0 +1,8 @@
+void fadeOffDueSegment(void) ;
+void programme(void) ;
+void blinkIndicator(void) ;
+void handleInputChange(uint8_t pin, bool state) ;
+void turnOn(uint8_t start, uint8_t leds_length, uint8_t segIdx) ;
+void turnOff(uint8_t start, uint8_t leds_length, uint8_t segIdx) ;
+void readAnalogPin(void) ;
+void intro(void);
